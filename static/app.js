8be2ac0cd253bac4893f42c0cfcb96bfa1d479a4
Yumarx <jumarpolanco@gmail.com>
2015-07-06 23:31:49 -0400

    initial commit

---
+++ static/app.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+angular.module('store')
+        .constant('SERVER_URL', '/api/products')
+        .factory('products', ['SERVER_URL', '$http', function (server, $http) {
+                var o = {products: []};
+                o.getAll = function (limit, skip, sort) {
+                    $http.get(server, {
+                        params: {
+                            limit: limit,
+                            skip: skip,
+                            sort: sort
+                        }
+                    })
+                    .success(function (data) {
+                        o.products = data;
+                    }).error(function (data) {
+                        o.products = [];
+                    });
+                };
+
+                return o;
+            }])
+        
