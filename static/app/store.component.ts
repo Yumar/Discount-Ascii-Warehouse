bff50b9cfbc9592e2496bb2573d4b06a3f6931e4
Yumarx <jumarpolanco@gmail.com>
2016-05-03 21:22:33 -0400

    init app

---
+++ static/app/store.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ascii-warehouse',
+  templateUrl: './static/app/store.html'
+})
+export class StoreComponent { }
+
---
+++ static/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {StoreComponent} from './app/store.component';
+
+bootstrap(StoreComponent);
